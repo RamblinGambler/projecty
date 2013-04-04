1a82f4cc9fd00dd4d905024312813b8185fb0eb8
Daniel McAdam <Daniel.gamble.mcadam@gmail.com>
2013-04-04 10:36:48 -0400

    AddedImage Upload Via URL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectx::Application.config.secret_token = 'ea257c88871a2429c5676906bb8897e8fe8dfd5d61fef0d84d65b19fbde551897b188c2dab72248a28db0242ad955624992ee3a555bc6431058050f0a99e89d3'
